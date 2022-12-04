66bb73251a42b823c814afc3f4d610d63937cf80
mxs <max.a.moreira@gmail.com>
2022-12-03 22:18:35 -0300

    Initial commit.

---
+++ src/main/java/com/mxs/voting/MxsVotingModuleApplication.java
@@ -0,0 +1,13 @@
+package com.mxs.voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxsVotingModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxsVotingModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mxs/voting/MxsVotingModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxs.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxsVotingModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

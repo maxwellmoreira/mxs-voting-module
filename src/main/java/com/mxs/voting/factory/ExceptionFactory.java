0c18bdee72d67ba63e1bb89cbd41bee84d25fb98
mxs <max.a.moreira@gmail.com>
2022-12-16 00:09:50 -0300

    Added exception handler.

--- src/main/java/com/mxs/voting/factory/ExceptionFactory.java
+++ src/main/java/com/mxs/voting/factory/ExceptionFactory.java
@@ -8,11 +8,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class ExceptionFactory {
     @ExceptionHandler(value = {InternalErrorException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ExceptionDto> resourceInternalErrorException(InternalErrorException internalErrorException, WebRequest webRequest) {
         ExceptionDto exceptionDto = new ExceptionDto();
         exceptionDto.setType(ExceptionType.INFRASTRUCTURE);
@@ -21,6 +23,7 @@ public class ExceptionFactory {
     }
 
     @ExceptionHandler(value = {NotFoundException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ExceptionDto> resourceNotFoundException(NotFoundException notFoundException, WebRequest webRequest) {
         ExceptionDto exceptionDto = new ExceptionDto();
         exceptionDto.setType(ExceptionType.BUSINESS);
